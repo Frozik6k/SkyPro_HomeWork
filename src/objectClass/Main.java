75a3dfa4691c3850ff388c65e51d629f8cef5cb4
Frozik6k <mgc2000@bk.ru>
2024-12-02 21:11:33 +0500

    Задачи по теме "Объекты и классы"

--- src/objectClass/Main.java
+++ src/objectClass/Main.java
@@ -2,6 +2,21 @@ package objectClass;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        Author maxPonomarev = new Author("Макс", "Пономарёв");
+
+        Author olgaChupina = new Author("Ольга", "Чупина");
+
+        Book chiefMain = new Book("Главный босс", maxPonomarev, 2022);
+
+        Book rollRecipes = new Book("Рецепты роллов", olgaChupina, 2020);
+
+        System.out.println("chiefMain = " + chiefMain);
+
+        System.out.println("rollRecipes = " + rollRecipes);
+
+        chiefMain.setYearPublic(2024);
+
+        System.out.println("chiefMain = " + chiefMain);
     }
 }
